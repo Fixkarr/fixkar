9a33ecaedebaf7b40635ebe55569c6f8de05eab4
princemaurya7746@gmail.com <princemaurya7746@gmail.com>
2026-01-13 18:12:01 +0530

    ewdx

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -10,8 +10,12 @@ import { FaTools, FaMapMarkerAlt, FaBolt } from "react-icons/fa";
 import { IoSearchCircle } from "react-icons/io5";
 import { useNavigate } from 'react-router-dom'
 import {Helmet} from 'react-helmet-async'
+import { useAuth } from '../context/AuthContext'
 const Home = () => {
   const navigate = useNavigate()
+  const auth = useAuth();
+  
+  console.log(auth);
   return (
    <>
   <Helmet>
