f5a5a2726286a8802944b37abe388c688618b03a
princemaurya7746@gmail.com <princemaurya7746@gmail.com>
2026-01-15 19:43:10 +0530

    efh

--- Frontend/src/Admin/AdminComponents/AdminSearch.jsx
+++ Frontend/src/Admin/AdminComponents/AdminSearch.jsx
@@ -209,7 +209,7 @@ const AdminSearch = ({ customers, professionals }) => {
                           {/* CENTER */}
                           <div className="col-md-4 text-center">
                             <span className="badge bg-info me-2">
-                              {p.profession}
+                              {p.profession.name}
                             </span>
 
                             <span
