249f2e2862367e823d0ddd32edc5f28c750821b7
princemaurya7746@gmail.com <princemaurya7746@gmail.com>
2026-01-17 13:01:10 +0530

    efh

--- Frontend/src/Admin/AdminComponents/AdminServices.jsx
+++ Frontend/src/Admin/AdminComponents/AdminServices.jsx
@@ -111,7 +111,7 @@ const AdminServices = () => {
                       {service.professionalCount} Pros
                     </span>
 
-                     <button className="btn btn-warning fw-semibold px-4 rounded-pill shadow-sm"
+                     <button className="badge bg-success px-3 py-2 rounded-pill"
             data-bs-toggle="modal"
             data-bs-target="#UpdateServiceModal"
           >
