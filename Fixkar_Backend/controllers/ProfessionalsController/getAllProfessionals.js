eefaf1399f1df746720204d21ae47f8c19e1cd1e
princemaurya7746@gmail.com <princemaurya7746@gmail.com>
2026-01-24 16:26:17 +0530

    abjfuogwb

--- Fixkar_Backend/controllers/ProfessionalsController/getAllProfessionals.js
+++ Fixkar_Backend/controllers/ProfessionalsController/getAllProfessionals.js
@@ -11,7 +11,15 @@ export const getAllProfessionals = async (req,res)=>{
         })
       }
 
-        const professionals = await Professional.find().select(`+bankDetails.bankName +bankDetails.holderName +bankDetails.accountNumber +bankDetails.ifsc +bankDetails.upi +bankDetails.panNumber +bankDetails.docPicUrl`).populate({
+        const professionals = await Professional.find().select({
+    "bankDetails.bankName": 1,
+    "bankDetails.holderName": 1,
+    "bankDetails.accountNumber": 1,
+    "bankDetails.ifsc": 1,
+    "bankDetails.upi": 1,
+    "bankDetails.panNumber": 1,
+    "bankDetails.docPicUrl": 1,
+  }).populate({
           path : "userId",
           model : "User",
           select : `
