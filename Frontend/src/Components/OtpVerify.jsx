30cb8e03df4fe0c35239e03db04b45b2b209f960
princemaurya7746@gmail.com <princemaurya7746@gmail.com>
2026-01-30 12:56:45 +0530

    abjfuogwb

--- Frontend/src/Components/OtpVerify.jsx
+++ Frontend/src/Components/OtpVerify.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "./Navbar";
-import "../css/forgetPass.css";
 import { ToastContainer, toast } from "react-toastify";
 import { useLocation, useNavigate } from "react-router-dom";
 import Countdown from "react-countdown";
