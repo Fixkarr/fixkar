5a0a09ef1ef22d97c5636b7b31c09a3c9c98db6d
princemaurya7746@gmail.com <princemaurya7746@gmail.com>
2026-01-30 11:01:25 +0530

    abjfuogwb

--- Fixkar_Backend/controllers/ProfessionalsController/getWalletTransaction.js
+++ Fixkar_Backend/controllers/ProfessionalsController/getWalletTransaction.js
@@ -1,23 +1,22 @@
-import { Professional } from "../../models/userModel.js"
-import { Wallet } from "../../models/walletModel.js";
-import { WalletTransaction } from "../../models/walletTransactionModel.js";
+    import { Professional } from "../../models/userModel.js"
+    import { Wallet } from "../../models/walletModel.js";
+    import { WalletTransaction } from "../../models/walletTransactionModel.js";
 
-export const getWalletTransaction = async (req, res) => {
-    try {
-        const professionalId = req.userId;
-        const professional = await Professional.findOne({ userId: professionalId })
+    export const getWalletTransaction = async (req, res) => {
+        try {
+            const professionalId = req.userId;
+            const professional = await Professional.findOne({ userId: professionalId })
 
-        const wallet = await Wallet.findOne({ professionalId : professional._id});
-        if (!wallet) return res.json([]);
+            const wallet = await Wallet.findOne({ professionalId : professional._id});
+            if (!wallet) return res.json([]);
 
-        const transactions = await WalletTransaction.find({
-            walletId: wallet._id,
-        })
-            .sort({ createdAt: -1 })
-            .populate("bookingId", "profession customerName status");
+            const transactions = await WalletTransaction.find({
+                walletId: wallet._id,
+            }).sort({ createdAt: -1, _id: -1 })
+                .populate("bookingId", "profession customerName status");
 
-        res.json({transactions});
-    } catch (error) {
-        res.status(500).json({ message: "Failed to fetch transactions" });
+            res.json({transactions});
+        } catch (error) {
+            res.status(500).json({ message: "Failed to fetch transactions" });
+        }
     }
-}
