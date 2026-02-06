c56d388d3c9a54a3cd4fd9ff887ed3901fd96b7c
princemaurya7746@gmail.com <princemaurya7746@gmail.com>
2026-02-06 16:42:45 +0530

    all clrat

--- Frontend/src/Pages/DashboardLayout.jsx
+++ Frontend/src/Pages/DashboardLayout.jsx
@@ -39,7 +39,7 @@ const DashboardLayout = () => {
 
   return (
     <>
-       {!isAuthenticated && <Navbar />}
+       {!isAuthenticated || !currentAdmin && <Navbar />}
       <div className="dashboardLayout d-flex">
 
            {isAuthenticated && isSidebarOpen && (
