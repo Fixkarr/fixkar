4d683c79385ff791b3e8296643f40eeadb2a8520
princemaurya7746@gmail.com <princemaurya7746@gmail.com>
2026-02-06 16:44:57 +0530

    all clrat

--- Frontend/src/Pages/DashboardLayout.jsx
+++ Frontend/src/Pages/DashboardLayout.jsx
@@ -50,14 +50,14 @@ const DashboardLayout = () => {
       )}
 
       {/* Sidebar */}
-      {isAuthenticated && <Sidebar isOpen={isSidebarOpen}  onClose={() => setIsSidebarOpen(false)}/>}
+      {isAuthenticated || currentAdmin && <Sidebar isOpen={isSidebarOpen}  onClose={() => setIsSidebarOpen(false)}/>}
 
 
       {/* Main Area */}
       <main className="dashboard-main bg-light">
 
         {/* ===== Top Bar ===== */}
-       {isAuthenticated &&  <section
+       {isAuthenticated || currentAdmin &&  <section
           className="dashboard-topbar"
         >
           {/* Gradient Header */}
