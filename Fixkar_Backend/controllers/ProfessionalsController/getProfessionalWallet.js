083b2327f29d5224fad0b5a1c871655992eb78f5
princemaurya7746@gmail.com <princemaurya7746@gmail.com>
2026-02-11 11:45:54 +0530

    all clrat

--- Fixkar_Backend/controllers/ProfessionalsController/getProfessionalWallet.js
+++ Fixkar_Backend/controllers/ProfessionalsController/getProfessionalWallet.js
@@ -4,19 +4,46 @@ import {Wallet} from '../../models/walletModel.js'
 export const getProfessionalWallet = async (req,res)=>{
     try {
         const userId = req.userId;
-        const professional = await Professional.findOne({userId})
+
+          const professional = await Professional.findOne({ userId })
+      .select(
+        "+bankDetails.accountNumber +bankDetails.holderName +bankDetails.ifsc"
+      );
+
 
         const wallet = await Wallet.findOne({professionalId : professional._id});
 
        
+  let maskedAccountNumber = null;
 
-        if(!wallet){
-            return res.json({
-            pendingBalance: 0,
-            totalEarned: 0,
-            totalWithdrawn : 0
-        })}
-        res.json({wallet});
+    if (professional?.bankDetails?.accountNumber) {
+      const acc = professional.bankDetails.accountNumber;
+      maskedAccountNumber = "XXXXXX" + acc.slice(-4);
+    }
+
+    const safeBankDetails = professional?.bankDetails
+      ? {
+          holderName: professional.bankDetails.holderName,
+          ifsc: professional.bankDetails.ifsc,
+          accountNumber: maskedAccountNumber,
+        }
+      : null;
+
+    if (!wallet) {
+      return res.json({
+        pendingBalance: 0,
+        totalEarned: 0,
+        totalWithdrawn: 0,
+        bankDetails: safeBankDetails,
+      });
+    }
+
+    res.json({
+      wallet,
+      bankDetails: safeBankDetails,
+    });
+
+    
     } catch (error) {
         res.status(500).json({ message: "Internal server error!" });
     }
