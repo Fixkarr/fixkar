4c2cb7baddd398a94debc30928c4f6ce75e47de0
princemaurya7746@gmail.com <princemaurya7746@gmail.com>
2026-05-20 15:18:55 +0530

    manage enquiry

--- Fixkar_Backend/controllers/Admin/AdminModels/contact.model.js
+++ Fixkar_Backend/controllers/Admin/AdminModels/contact.model.js
@@ -3,7 +3,8 @@ const contactSchema = new mongoose.Schema({
     name : {type : String, required : true},
     email : {type : String, required : true},
     phone : {type : String, required : true},
-    message : {type : String, required : true}
+    message : {type : String, required : true},
+    replied : {type : Boolean, default : false}
 }, {timestamps : true})
 
 export const Contact = mongoose.model("Contact", contactSchema);
--- Fixkar_Backend/controllers/contact.controller.js
+++ Fixkar_Backend/controllers/contact.controller.js
@@ -9,7 +9,7 @@ export const sendEnquiry = async (req,res)=>{
             })
         }
 
-        const isEnquiryExists = await Contact.find({email,phone});
+        const isEnquiryExists = await Contact.find({email,phone, replied : false});
         if(isEnquiryExists.length > 0){
             return res.status(400).json({
                 message : "You have already sent an enquiry. We will get back to you soon!"
@@ -28,5 +28,34 @@ export const sendEnquiry = async (req,res)=>{
         return res.status(500).json({
             message : "Internal server error"
         });
+    }
+}
+
+export const getEnquiries = async (req,res)=>{
+    try {
+        const admin = req.admin;
+        if(!admin){
+            return res.status(400).json({
+                message : "Unauthorized!"
+            })
+        }
+
+        const enquiries = await Contact.find();
+        if(enquiries.length <= 0){
+            return res.status(400).json({
+                message : "No Enquiries left!"
+            })
+        }
+
+        return res.status(200).json({
+            message : "Enquiries fetched!",
+            enquiries,
+            enquiriesCount : enquiries.length
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            message : "Internal Server Error!"
+        })
     }
 }
