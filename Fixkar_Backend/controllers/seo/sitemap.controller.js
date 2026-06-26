a6e79a7bc36ee15c3d4369982538636729bcddfa
princemaurya7746@gmail.com <princemaurya7746@gmail.com>
2026-06-26 12:17:50 +0530

    add slug

--- Fixkar_Backend/controllers/seo/sitemap.controller.js
+++ Fixkar_Backend/controllers/seo/sitemap.controller.js
@@ -1,5 +1,27 @@
 import { Professional } from "../../models/userModel.js";
 
+export const sitemapIndex = async (req, res) => {
+  try {
+    const baseUrl = process.env.FRONTEND_URL || "https://fixkarr.com";
+
+    const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+
+  <sitemap>
+    <loc>${baseUrl}/sitemap-professionals.xml</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+  </sitemap>
+
+</sitemapindex>`;
+
+    res.header("Content-Type", "application/xml");
+    res.status(200).send(xml);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Failed to generate sitemap index");
+  }
+};
+
 
 export const professionalSitemap = async (req, res) => {
      try {
