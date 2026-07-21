70ce493625d899575ef8232aab339cf6ba89f2e8
princemaurya7746@gmail.com <princemaurya7746@gmail.com>
2026-07-21 18:14:21 +0530

    seo

--- Fixkar_Backend/controllers/seo/generateSitemap.js
+++ Fixkar_Backend/controllers/seo/generateSitemap.js
@@ -1,3 +1,17 @@
+import {Professional} from "../../models/userModel.js"
 export const sitemapController = async (req,res) =>{
-    res.send("Sitemap working!")
+    try {
+    const professionals = await Professional.find({
+      status: "approved",
+    })
+      .select("_id slug updatedAt")
+      .lean();
+
+    res.json(professionals);
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 }
