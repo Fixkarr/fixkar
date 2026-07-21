5354096ae588a70608d9fb01e8cf130d31671822
princemaurya7746@gmail.com <princemaurya7746@gmail.com>
2026-07-21 18:21:50 +0530

    add slug

--- Fixkar_Backend/controllers/seo/generateSitemap.js
+++ Fixkar_Backend/controllers/seo/generateSitemap.js
@@ -7,7 +7,30 @@ export const sitemapController = async (req,res) =>{
       .select("_id slug updatedAt")
       .lean();
 
-    res.json(professionals);
+      const urls = professionals
+      .map((professional) => {
+        return `
+<url>
+  <loc>https://www.fixkarr.com/professional/profile/visit/${professional._id}/${professional.slug}</loc>
+  <lastmod>${professional.updatedAt.toISOString()}</lastmod>
+  <changefreq>weekly</changefreq>
+  <priority>0.9</priority>
+</url>`;
+      })
+      .join("");
+
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+
+<urlset
+xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+
+${urls}
+
+</urlset>`;
+
+    res.header("Content-Type", "application/xml");
+    res.send(sitemap);
+
   } catch (error) {
     res.status(500).json({
       success: false,
