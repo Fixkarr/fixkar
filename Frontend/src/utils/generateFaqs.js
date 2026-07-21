9295650262b9078f6a317fa660a1a4682eb671e0
princemaurya7746@gmail.com <princemaurya7746@gmail.com>
2026-07-21 17:46:01 +0530

    seo

--- Frontend/src/utils/generateFaqs.js
+++ Frontend/src/utils/generateFaqs.js
@@ -103,6 +103,6 @@ export const generateAbout = (professionalInfo, id) => {
         description
             ? `${name} says - ${description}` 
             : ""
-    } Customers can hire ${name} through Fixkar Smart Solutions for reliable ${profession?.toLowerCase()} services. They can visit https://www.fixkarr.com/professional/profile/visit/${id}/${professionalInfo.slug} to view the profile and book ${name} for your ${profession?.toLowerCase()} needs. Visit fixkarr.com for more information and to explore other verified professionals in your area.`;
+    } Customers can hire ${name} through Fixkar Smart Solutions for reliable ${profession?.toLowerCase()} services. They can visit https://www.fixkarr.com/professional/profile/visit/${id}/${professionalInfo?.slug} to view the profile and book ${name} for your ${profession?.toLowerCase()} needs. Visit fixkarr.com for more information and to explore other verified professionals in your area.`;
 
 }
