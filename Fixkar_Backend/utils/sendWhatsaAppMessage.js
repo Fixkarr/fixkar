97e95a76f7bc8d268ad653c286fc3864440933a0
princemaurya7746@gmail.com <princemaurya7746@gmail.com>
2026-07-24 21:06:24 +0530

    sdsdv

--- Fixkar_Backend/utils/sendWhatsaAppMessage.js
+++ Fixkar_Backend/utils/sendWhatsaAppMessage.js
@@ -54,7 +54,7 @@ export const sendWhatsAppMessage = async (phone, customerName, address, bookingI
       }
     );
 
-    console.log(response.data);
+    return response.data;
   } catch (error) { 
     console.log(error.response?.data || error.message);
   }
