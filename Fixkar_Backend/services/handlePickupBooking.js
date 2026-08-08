9c1987fad521051621e3b2a57ac60ff344d5a789
princemaurya7746@gmail.com <princemaurya7746@gmail.com>
2026-08-08 12:09:00 +0530

    sdcsd

--- Fixkar_Backend/services/handlePickupBooking.js
+++ Fixkar_Backend/services/handlePickupBooking.js
@@ -24,6 +24,14 @@ export const handlePickupBooking = async ({
     audioMessages,
 }) => {
     try {
+        console.log("PICKUP LOCATION:", {
+    customerLat,
+    customerLng,
+    customerLatNumber: Number(customerLat),
+    customerLngNumber: Number(customerLng),
+    serviceId: service?._id,
+    taskId: task?._id,
+});
         const eligibleProfessionals =
             await findEligibleProfessionals({
                 serviceId: service._id,
