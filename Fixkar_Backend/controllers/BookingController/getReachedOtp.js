4b23254691ea85c29986259bc9850a333300045b
Prince Maurya <princemaurya7746@gmail.com>
2026-08-12 20:07:00 +0530

    security: restrict reached OTP access to booking customer

--- Fixkar_Backend/controllers/BookingController/getReachedOtp.js
+++ Fixkar_Backend/controllers/BookingController/getReachedOtp.js
@@ -1,16 +1,35 @@
+import { Booking } from "../../models/bookingModel.js";
+import { Customer } from "../../models/userModel.js";
 import { ReachedOtp } from "../../models/reachedOtpModel.js";
 
-export const getReachedOtp = async (req,res)=>{
+export const getReachedOtp = async (req, res) => {
     try {
-        const {bookingId} = req.params; 
-        const reachedOtpRecord = await ReachedOtp.findOne({bookingId});
+        const { bookingId } = req.params;
+        const customer = await Customer.findOne({ userId: req.userId }).select("_id");
 
-        if(!reachedOtpRecord){
-            return res.status(404).json({message: "Reached OTP not found"});
+        if (!customer) {
+            return res.status(403).json({ message: "Customer access required" });
         }
 
-        return res.status(200).json({otp: reachedOtpRecord.otp});
+        const booking = await Booking.findOne({
+            _id: bookingId,
+            customerId: customer._id,
+            status: "reached",
+        }).select("_id");
+
+        if (!booking) {
+            return res.status(404).json({ message: "Booking not found or OTP is not available" });
+        }
+
+        const reachedOtpRecord = await ReachedOtp.findOne({ bookingId: booking._id });
+
+        if (!reachedOtpRecord) {
+            return res.status(404).json({ message: "Reached OTP not found" });
+        }
+
+        return res.status(200).json({ otp: reachedOtpRecord.otp });
     } catch (error) {
-        res.status(500).json({message: "Server Error"});
+        console.error("getReachedOtp error:", error);
+        return res.status(500).json({ message: "Server Error" });
     }
-}
+};
