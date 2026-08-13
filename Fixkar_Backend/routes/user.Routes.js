634cecbd5025c7d1e51ad7c68a8ee0de018abd34
Prince Maurya <princemaurya7746@gmail.com>
2026-08-13 08:50:00 +0530

    feat: expose coupon based user routes

--- Fixkar_Backend/routes/user.Routes.js
+++ Fixkar_Backend/routes/user.Routes.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express from 'express'
 import { getCurrentUser } from '../controllers/user.controller.js';
 import { isAuth } from '../middlewares/isAuth.js';
 import { onboard } from '../controllers/ProfessionalsController/onboardController.js';
@@ -20,15 +20,12 @@ import { getTransaction } from '../controllers/ProfessionalsController/getTransa
 import { sendWithDrawRequest } from '../controllers/ProfessionalsController/sendWithdrawRequest.js';
 import { saveFormResponse } from '../controllers/saveFormResponse.controller.js';
 import { getBanks } from '../controllers/Admin/AdminController/bank.controller.js';
-import { getEligibleOffers } from '../controllers/CustomerController/getEligibleOffers.controller.js';
-import { applyOfferToBooking } from '../controllers/CustomerController/applyOfferToBooking.js';
 import { getMyAnnouncements } from '../controllers/getMyAnnouncements.js';
 import { sendEnquiry } from '../controllers/contact.controller.js';
 import { getProfessionalPickupRequests } from '../controllers/ProfessionalsController/getPickupRequests.controller.js';
 import { acceptPickupRequest, rejectPickupRequest } from '../controllers/ProfessionalsController/pickup.controller.js';
+import { claimCouponController, validateCouponController, applyCouponToBookingController, getMyCouponClaims } from '../controllers/CouponController/coupon.controller.js';
 const userRoute = express.Router();
-
-// /api/user
 
 userRoute.get("/current", isAuth ,getCurrentUser);
 userRoute.get("/professionals", isAuth, isAdmin, getAllProfessionals);
@@ -37,58 +34,36 @@ userRoute.get("/getUserById/:userId", getUserById);
 userRoute.get("/professionals/search", searchProfessionals);
 userRoute.get("/get-service-skills/:serviceId", getServiceSkills)
 
-
-// professionals routes
-
 userRoute.post("/onboard", isAuth, upload.fields([
     { name: "profilePicture", maxCount: 1 },
     { name: "poi", maxCount: 1 },
   ]), multerErrorHandler, onboard);
 
-  
+userRoute.post("/professional/complete-profile", isAuth, completeProfile);
+userRoute.post("/professional/set-busy-days", isAuth, setBusyDays)
+userRoute.post('/professional/bank-details', isAuth, upload.single("passbookImage"), multerErrorHandler, bankDetails)
+userRoute.get('/professional/get-transactions/:proId', isAuth, getTransaction)
+userRoute.post('/professional/send-withdrawn-request', isAuth, sendWithDrawRequest);
 
- userRoute.post("/professional/complete-profile", isAuth, completeProfile);
- userRoute.post("/professional/set-busy-days", isAuth, setBusyDays)
- userRoute.post('/professional/bank-details', isAuth, upload.single("passbookImage"), multerErrorHandler, bankDetails)
- userRoute.get('/professional/get-transactions/:proId', isAuth, getTransaction)
- userRoute.post('/professional/send-withdrawn-request', isAuth, sendWithDrawRequest);
-
-
-// update profile
-userRoute.post("/update-profile-picture", isAuth, upload.fields([
-    {name : "profilePicture", maxCount : 1 }
-  ]), multerErrorHandler, updateProfilePicture)
-
-
+userRoute.post("/update-profile-picture", isAuth, upload.fields([{name : "profilePicture", maxCount : 1 }]), multerErrorHandler, updateProfilePicture)
 userRoute.post("/update-profile-info", isAuth, updateProfileInfo)
-
 userRoute.post('/professional/update-skills', isAuth, updateSkills)
-
-//gallery
 userRoute.post('/upload-media', isAuth, uploadMedia)
 userRoute.get('/signature', isAuth, getCloudinarySignature);
 userRoute.delete('/delete-media/:mediaId', isAuth, deleteMedia)
 
-//service
-
 userRoute.get('/get-services', getServices)
-
 userRoute.post('/save-form-response', isAuth, saveFormResponse);
 userRoute.get('/get-banks', isAuth, getBanks);
 
-//offers
-userRoute.get('/get-elligible-offers/:bookingId', isAuth, getEligibleOffers)
-userRoute.post('/apply-offer', isAuth, applyOfferToBooking);
+// Coupon system: no endpoint returns all active offers to the user.
+userRoute.post('/coupons/validate', isAuth, validateCouponController);
+userRoute.post('/coupons/claim', isAuth, claimCouponController);
+userRoute.get('/coupons/my-claims', isAuth, getMyCouponClaims);
+userRoute.post('/coupons/apply-to-booking', isAuth, applyCouponToBookingController);
 
-
-// announcement
 userRoute.get('/get-my-announcements', isAuth, getMyAnnouncements);
-
-//contact
 userRoute.post('/send-enquiry', sendEnquiry);
-
-
-//pickup request routes
 
 userRoute.get('/professional/pickup-requests', isAuth, getProfessionalPickupRequests)
 userRoute.post('/professional/pickup-request-accept', isAuth, acceptPickupRequest)
