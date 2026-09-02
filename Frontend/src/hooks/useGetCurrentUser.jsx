660a3ea0273fbec33a3bcb51eb4b4a4f93a0b932
princemaurya7746@gmail.com <princemaurya7746@gmail.com>
2026-09-02 18:52:47 +0530

    sacs

--- Frontend/src/hooks/useGetCurrentUser.jsx
+++ Frontend/src/hooks/useGetCurrentUser.jsx
@@ -4,6 +4,7 @@ import { server_url } from '../App'
 import { useDispatch } from 'react-redux'
 import { setAuthLoading, setCurrentUserData } from '../redux/user.slice'
 import { toast } from 'react-toastify'
+import { setSelectedLocation } from '../redux/location.slice'
 
 const useGetCurrentUser = () => {
     const dispatch = useDispatch()
@@ -12,7 +13,10 @@ const useGetCurrentUser = () => {
            try {
             dispatch(setAuthLoading(true))
              const result = await axios.get(`${server_url}/api/user/current`, {withCredentials : true})
-            dispatch(setCurrentUserData(result.data)); 
+            dispatch(setCurrentUserData(result.data));
+            if (result.data?.selectedLocation) {
+                dispatch(setSelectedLocation(result.data.selectedLocation));
+                }
            } catch (error) {
                dispatch(setCurrentUserData(null))
            }
