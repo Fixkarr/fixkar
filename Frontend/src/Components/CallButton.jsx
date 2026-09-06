5ceb6081227964f43c86dfef92a7c0318fcea70f
princemaurya7746@gmail.com <princemaurya7746@gmail.com>
2026-09-06 15:07:36 +0530

    assd

--- Frontend/src/Components/CallButton.jsx
+++ Frontend/src/Components/CallButton.jsx
@@ -1,7 +1,7 @@
 import { IoCallOutline } from "react-icons/io5";
 import { useLocation, useNavigate } from "react-router-dom";
 import ProfessionalAchievementBadge from "./ProfessionalAchievementBadge";
-import '../css/professional-public-profile.css'
+import '../Customer/professional-public-profile.css'
 
 const CallButton = ({ currentUserData, professionalInfo }) => {
   const navigate = useNavigate();
