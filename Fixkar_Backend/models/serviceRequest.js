03de05bf52c50ce6109704382123faf47de3e256
princemaurya7746@gmail.com <princemaurya7746@gmail.com>
2026-09-07 18:36:22 +0530

    sdvsd

--- Fixkar_Backend/models/serviceRequest.js
+++ Fixkar_Backend/models/serviceRequest.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
 const serviceRequestSchema = new mongoose.Schema({
-    professiona : {type : mongoose.Schema.Types.ObjectId, ref : "Professional", required : true},
+    professional : {type : mongoose.Schema.Types.ObjectId, ref : "Professional", required : true},
     serviceName : {type : String, required : true},
     description : {type : String, required : true},
     status : {type : String, enum : ["pending_review", "approved", "rejected"], default : "pending_review"},
