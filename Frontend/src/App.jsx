bbc3d9d76200c12f7950f12c8e93ae8abd38be0d
princemaurya7746@gmail.com <princemaurya7746@gmail.com>
2026-09-11 01:19:25 +0530

    fsd

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -517,8 +517,8 @@ socket.on("pickupProfessionalAccepted", (data) => {
         {/* Professional */}
           <Route element={<ProtectedRoute 
           allowedRole="professional"
-          requireOnboarded={isOnboarded}
-          requireMobileVerified={isMobileVerified}/>
+          requireOnboarded={true}
+          requireMobileVerified={true}/>
           }>
                <Route
           path="/professional/home"
